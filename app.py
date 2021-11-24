2524c6e18692848ea3add748a02df8a1030cf7ba
Suzy Q <blakmagjick@users.noreply.github.com>
2021-11-23 19:43:57 -0800

    Added User model and Register route. Tested and working in Postman

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 
 import models
+from resources.users import users
 
+from flask_login import LoginManager
+
+import os
 from dotenv import load_dotenv 
 load_dotenv()
 
@@ -10,9 +14,25 @@ PORT=8000
 
 app = Flask(__name__) 
 
+app.secret_key = os.environ.get('FLASK_APP_SECRET')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        user = models.User.get_by_id(user_id)
+        return user
+    except models.DoesNotExist:
+        return None
+
+app.register_blueprint(users, url_prefix='/users')
+
 @app.route('/')
 def test():
     return 'Server connected'
 
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
---
+++ resources/users.py
@@ -0,0 +1,43 @@
+import models
+
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import login_user, current_user, logout_user
+from playhouse.shortcuts import model_to_dict
+
+users = Blueprint('users', 'users')
+
+@users.route('/register', methods=['POST'])
+def register():
+    payload = request.get_json()
+    payload['email'] = payload['email'].lower()
+    payload['username'] = payload['username'].lower()
+
+    try:
+        models.User.get(models.User.username == payload['username'])
+
+        return jsonify(
+            data = {},
+            message=f"A user with the username {payload['username']} already exists",
+            status=401
+        ), 401
+
+    except models.DoesNotExist:
+        pw_hash = generate_password_hash(payload['password'])
+        created_user = models.User.create(
+            username=payload['username'],
+            email=payload['email'],
+            password=pw_hash
+        )
+
+        login_user(created_user)
+
+        created_user_dict = model_to_dict(created_user)
+
+        created_user_dict.pop('password')
+
+        return jsonify(
+            data=created_user_dict,
+            message=f"Successfully registered user {created_user_dict['username']}",
+            status=201
+        ), 201
