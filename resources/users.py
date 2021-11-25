5dea970fd4958d500d6e3350035f2829b03cfacf
Suzy Q <blakmagjick@users.noreply.github.com>
2021-11-24 17:32:28 -0800

    Added Login route, tested and working

--- resources/users.py
+++ resources/users.py
@@ -41,3 +41,46 @@ def register():
             message=f"Successfully registered user {created_user_dict['username']}",
             status=201
         ), 201
+
+@users.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    payload['username'] = payload['username'].lower()
+
+    try:
+        user = models.User.get(models.User.username == payload['username'])
+
+        user_dict = model_to_dict(user)
+
+        password_is_good = check_password_hash(user_dict['password'], payload['password'])
+
+        if (password_is_good):
+            login_user(user)
+
+            user_dict.pop('password')
+
+            return jsonify (
+                data=user_dict,
+                message=f"Successfully logged in as {user_dict['username']}",
+                status=200
+            ), 200
+        else:
+            print('Email is no good')
+
+            return jsonify (
+                data={},
+                message='Email or password is incorrect',
+                status=401
+            ), 401
+
+    except models.DoesNotExist:
+        print('Email not found')
+
+        return jsonify(
+            data={},
+            message='Email or password is incorrect',
+            status=401
+        ), 401
+
+
+
