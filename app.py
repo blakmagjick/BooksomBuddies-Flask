2ed288e810dc3536447e9b3d0a88577b69cc6d8b
Suzy Q <blakmagjick@users.noreply.github.com>
2021-12-17 19:00:31 -0800

    Added cookie_secure true to try and fix Safari cookies issue

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ PORT=8000
 app = Flask(__name__) 
 
 app.secret_key = os.environ.get('FLASK_APP_SECRET')
-# app.config['SESSION_COOKIE_SAMESITE'] = "None"
-# app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = "None"
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['COOKIE_SECURE'] = True
 
 login_manager = LoginManager()
 login_manager.init_app(app)
